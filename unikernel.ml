697d3a69790ba40450b270d8947e1d417add09b2
Magnus Skjegstad <magnus@skjegstad.com>
2015-01-28 22:24:22 +0000

    Support for different modes

--- unikernel.ml
+++ unikernel.ml
@@ -23,13 +23,13 @@ module Main (C: V1_LWT.CONSOLE) (Netif : V1_LWT.NETWORK) = struct
   module Socks = Socks.SOCKS4 (Stack)
 
   type flowpair = {
-    client : Stack.T.flow;
-    socks : Stack.T.flow;
+    incoming : Stack.T.flow;
+    outgoing : Stack.T.flow;
   }
 
   type list_of_flowpairs = flowpair list
 
-  type t = {
+  type socks_t = {
     socks_ip : Ipaddr.V4.t;
     socks_port : int;
     dest_ip : Ipaddr.V4.t;
@@ -49,66 +49,66 @@ module Main (C: V1_LWT.CONSOLE) (Netif : V1_LWT.NETWORK) = struct
     | [] -> []
     | hd :: tl ->
       let new_tl = drop_flowpair tl fp in
-      if (hd.client == fp.client) && (hd.socks == fp.socks) then new_tl else hd :: new_tl
+      if (hd.incoming == fp.incoming) && (hd.outgoing == fp.outgoing) then new_tl else hd :: new_tl
 
 
   let rec find_flowpairs_by_flow (flowpairs : list_of_flowpairs) (flow : Stack.T.flow)  =
     match flowpairs with
     | [] -> []
     | hd :: tl -> 
-      if (hd.client == flow) || (hd.socks == flow) then 
+      if (hd.incoming == flow) || (hd.outgoing == flow) then 
         [hd] @ find_flowpairs_by_flow tl flow
       else 
         find_flowpairs_by_flow tl flow
 
-  let rec report_and_close_pairs context c fps message =
+  let rec report_and_close_pairs flowpairs c fps message =
     C.log c message;
     match fps with
     | [] -> Lwt.return_unit
     | hd :: tl -> 
-      context.flowpairs := (drop_flowpair !(context.flowpairs) hd);
+      flowpairs := (drop_flowpair !(flowpairs) hd);
       Lwt.join [ 
-        Stack.T.close hd.client ;
-        Stack.T.close hd.socks ] >>= fun () ->
-      report_and_close_pairs context c tl message
+        Stack.T.close hd.incoming ;
+        Stack.T.close hd.outgoing ] >>= fun () ->
+      report_and_close_pairs flowpairs c tl message
 
-  let report_and_close_flow context c flow message =
+  let report_and_close_flow flowpairs c flow message =
     C.log c message;
-    let fp = find_flowpairs_by_flow !(context.flowpairs) flow in
+    let fp = find_flowpairs_by_flow !(flowpairs) flow in
     match fp with
     | [] -> Lwt.return_unit
-    | l -> report_and_close_pairs context c l "Flow pair found - closing..."
+    | l -> report_and_close_pairs flowpairs c l "Flow pair found - closing..."
 
-  let write_with_check context c flow buf =
+  let write_with_check flowpairs c flow buf =
     Stack.T.write flow buf >>= fun result -> 
     match result with 
-    | `Eof -> report_and_close_flow context c flow "Unable to write to flow (eof)"
-    | `Error e -> report_and_close_flow context c flow (error_message e)
+    | `Eof -> report_and_close_flow flowpairs c flow "Unable to write to flow (eof)"
+    | `Error e -> report_and_close_flow flowpairs c flow (error_message e)
     | `Ok _ -> Lwt.return_unit
 
-  let rec read_and_forward context c input_flow output_flow  =
+  let rec read_and_forward flowpairs c input_flow output_flow  =
     Stack.T.read input_flow >>= fun result -> 
     match result with  
-    | `Eof -> report_and_close_flow context c input_flow "Closing connection (eof)"
-    | `Error e -> report_and_close_flow context c input_flow (error_message e)
-    | `Ok buf -> write_with_check context c output_flow buf >>= fun () -> read_and_forward context c input_flow output_flow
+    | `Eof -> report_and_close_flow flowpairs c input_flow "Closing connection (eof)"
+    | `Error e -> report_and_close_flow flowpairs c input_flow (error_message e)
+    | `Ok buf -> write_with_check flowpairs c output_flow buf >>= fun () -> read_and_forward flowpairs c input_flow output_flow
 
-  let connect context c s dest_server_port input_flow =
+  let connect_socks context c s dest_server_port input_flow =
     C.log c "New incoming connection - Forwarding connection through SOCKS";
     Stack.T.create_connection (Stack.tcpv4 s) (context.socks_ip, context.socks_port) >>= fun socks_con -> 
     match socks_con with 
     | `Error e -> C.log c (Printf.sprintf "Unable to connect to SOCKS server. Closing input flow. Error %s" (error_message e)); Stack.T.close input_flow
     | `Ok socks_flow -> 
       C.log c (Printf.sprintf "Connected to SOCKS ip %s port %d" (Ipaddr.V4.to_string (context.socks_ip)) context.socks_port);
-      context.flowpairs := [{client=input_flow; socks=socks_flow}] @ !(context.flowpairs);
+      context.flowpairs := [{incoming=input_flow; outgoing=socks_flow}] @ !(context.flowpairs);
       C.log c (Printf.sprintf "Connecting to dest ip %s port %d through SOCKS" (Ipaddr.V4.to_string (context.dest_ip)) dest_server_port);
       Socks.connect socks_flow "mirage" (context.dest_ip) dest_server_port >>= fun result ->
       match result with
-      | `Eof -> report_and_close_flow context c socks_flow "Eof while speaking to SOCKS"
-      | `Error e -> C.log c "Connection through SOCKS failed" ; report_and_close_flow context c socks_flow (error_message e)
+      | `Eof -> report_and_close_flow context.flowpairs c socks_flow "Eof while speaking to SOCKS"
+      | `Error e -> C.log c "Connection through SOCKS failed" ; report_and_close_flow context.flowpairs c socks_flow (error_message e)
       | `Ok -> C.log c "Connection succeeded. Forwarding."; Lwt.choose [
-          read_and_forward context c input_flow socks_flow;
-          read_and_forward context c socks_flow input_flow
+          read_and_forward context.flowpairs c input_flow socks_flow;
+          read_and_forward context.flowpairs c socks_flow input_flow
         ]
 
   (* from mirage-skeleton *)
@@ -119,7 +119,20 @@ module Main (C: V1_LWT.CONSOLE) (Netif : V1_LWT.NETWORK) = struct
     | `Ok t -> return t 
 
   let start c n = 
-    Printf.printf "Accepted parameters in extra= are: socks_ip=[ipv4] socks_port=[port] dest_ip=[ipv4 relative to socks endpoint] dest_ports=[port1,port2...] ip=[local ip] netmask=[local netmask] gw=[local gw]\n";
+    (* show help on boot *)
+    Printf.printf "*** mirage-mimic supported boot options ***\n";
+    Printf.printf "Accepted parameters in extra= are: \n";
+    Printf.printf "\tmode=[tcp,socks]\n";
+    Printf.printf "\tip=[local ip]\n";
+    Printf.printf "\tnetmask=[local netmask]\n";
+    Printf.printf "\tgw=[local gw]\n";
+    Printf.printf "\tports=[port1,...portN] (ports to listen to)\n";
+    Printf.printf "In socks mode:\n";
+    Printf.printf "\tsocks_ip=[ipv4]\n";
+    Printf.printf "\tsocks_port=[port]\n";
+    Printf.printf "\tdest_ip=[destination ipv4 relative to socks endpoint]\n";
+    Printf.printf "*****\n%!"; 
+
     Bootvar.create >>= fun bootvar ->
     let ip = Ipaddr.V4.of_string_exn (Bootvar.get bootvar "ip") in
     let netmask = Ipaddr.V4.of_string_exn (Bootvar.get bootvar "netmask") in
@@ -133,17 +146,30 @@ module Main (C: V1_LWT.CONSOLE) (Netif : V1_LWT.NETWORK) = struct
     } in
     or_error "stack" Stack.connect stack_config >>= fun s ->
     (* set up context, socks config etc *)
-    let dest_ip = Ipaddr.V4.of_string_exn (Bootvar.get bootvar "dest_ip") in
-    let socks_ip = Ipaddr.V4.of_string_exn (Bootvar.get bootvar "socks_ip") in
-    let socks_port = int_of_string (Bootvar.get bootvar "socks_port") in
     let dest_ports = 
-      let ports = Re_str.(split (regexp_string ",") (Bootvar.get bootvar "dest_ports")) in
+      let ports = Re_str.(split (regexp_string ",") (Bootvar.get bootvar "ports")) in
       List.map int_of_string ports
     in
-    let context : t = { socks_port = socks_port; socks_ip = socks_ip; dest_ip = dest_ip; dest_ports = dest_ports; flowpairs = ref [] } in
-    (* listen to ports from dest_ports *)
-    let begin_listen port = Stack.listen_tcpv4 s ~port:port (connect context c s port); Printf.printf "Listening to port %d\n" port in
-    List.iter begin_listen (context.dest_ports);
-    Stack.listen s
-
+    let mode = 
+            let mode_str = Bootvar.get bootvar "mode" in
+            (if mode_str = "tcp" then `TCP 
+            else if mode_str = "socks" then `SOCKS 
+            else `UNKNOWN) in
+    match mode with 
+    | `SOCKS -> begin
+            let dest_ip = Ipaddr.V4.of_string_exn (Bootvar.get bootvar "dest_ip") in
+            let socks_ip = Ipaddr.V4.of_string_exn (Bootvar.get bootvar "socks_ip") in
+            let socks_port = int_of_string (Bootvar.get bootvar "socks_port") in
+            let context : socks_t = { socks_port = socks_port; socks_ip = socks_ip; dest_ip = dest_ip; dest_ports = dest_ports; flowpairs = ref [] } in
+            (* listen to ports from dest_ports *)
+            let begin_listen port = Stack.listen_tcpv4 s ~port:port (connect_socks context c s port); Printf.printf "Listening to port %d\n" port in
+            List.iter begin_listen (context.dest_ports);
+            Stack.listen s
+    end
+    | `TCP -> begin
+            Printf.printf "TCP not supported yet.%!"; return_unit
+    end
+    | `UNKNOWN -> begin
+            Printf.printf "Mode unknown or mode boot parameter not set%!."; return_unit
+    end
 end
