f8fb00569abae4e01a071ee1694d5533ad9c7662
heidi-ann <hh360@cam.ac.uk>
2015-01-28 14:42:07 +0000

    add dhcp as an option

--- config.ml
+++ config.ml
@@ -2,9 +2,29 @@ open Mirage
 
 let main = foreign "Unikernel.Main" (console @-> network @-> job)
 
+
+let net =
+  try match Sys.getenv "NET" with
+    | "direct" -> `Direct
+    | "socket" -> `Socket
+    | _ -> `Direct
+  with Not_found -> `Direct
+
+let dhcp =
+  try match Sys.getenv "DHCP" with
+    | "" -> false
+    | _ -> true
+  with Not_found -> false
+
+let stack =
+  match net, dhcp with
+  | `Direct, true -> direct_stackv4_with_dhcp default_console tap0
+  | `Direct, false -> direct_stackv4_with_default_ipv4 default_console tap0
+  | `Socket, _ -> socket_stackv4 default_console [Ipaddr.V4.any]
+
 let () =
-  add_to_ocamlfind_libraries ["cstruct"; "cstruct.syntax"; "re"; "re.str"; "tcpip.ethif"; "tcpip.tcp"; "tcpip.udp"; "tcpip.stack-direct" ];
-  add_to_opam_packages ["cstruct"; "tcpip"; "re"];
+  add_to_ocamlfind_libraries ["cstruct"; "cstruct.syntax"; "re"; "re.str"; "tcpip.ethif"; "tcpip.tcp"; "tcpip.udp"; "tcpip.stack-direct"; "mirage-http" ];
+  add_to_opam_packages ["cstruct"; "tcpip"; "re"; "mirage-http"];
   register "unikernel" [
-    main $ default_console $ tap0
+    main $ default_console $ stack
   ]
