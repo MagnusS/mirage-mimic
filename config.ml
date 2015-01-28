e442e91b85e6b3a6034c4322b0093012b7229760
heidi-ann <hh360@cam.ac.uk>
2015-01-28 15:05:15 +0000

    reverting to tap0 to debug

--- config.ml
+++ config.ml
@@ -6,5 +6,5 @@ let () =
   add_to_ocamlfind_libraries ["cstruct"; "cstruct.syntax"; "re"; "re.str"; "tcpip.ethif"; "tcpip.tcp"; "tcpip.udp"; "tcpip.stack-direct" ];
   add_to_opam_packages ["cstruct"; "tcpip"; "re"];
   register "unikernel" [
-    main $ default_console $ (socket_stackv4 default_console [Ipaddr.V4.any])
+    main $ default_console $ tap0
   ]
